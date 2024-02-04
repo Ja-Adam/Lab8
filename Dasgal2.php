3aedfebcd2d66b02753ec1c3105da21aee9402f3
Ja-Adam <jadambajaak874@gmail.com>
2024-02-05 02:49:25 +0800

    commit

--- Dasgal2.php
+++ Dasgal2.php
@@ -2,77 +2,77 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dropdown Item Exchange</title>
-    <style>
-        select {
-            width: 150px;
-            margin: 10px;
-            height: 150px;
-        }
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dropdown Item Exchange</title>
+  <style>
+    select {
+      width: 150px;
+      margin: 10px;
+      height: 150px;
+    }
 
-        button {
-            display: block;
-            background-color: #C0C0C0;
-            margin-top: 10px;
-        }
-    </style>
+    button {
+      display: block;
+      background-color: #C0C0C0;
+      margin-top: 10px;
+    }
+  </style>
 </head>
 
 <body>
 
-    <?php
-    session_start();
+  <?php
+  session_start();
 
-    $list1 = ['Алим', 'Гадил', 'Жүрж', 'Тарвас', 'Манго'];
-    $list2 = isset($_SESSION['list2']) ? $_SESSION['list2'] : [];
+  $list1 = ['Алим', 'Гадил', 'Жүрж', 'Тарвас', 'Манго'];
+  $list2 = isset($_SESSION['list2']) ? $_SESSION['list2'] : [];
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST['moveToSecond'])) {  // 2дох хүснэгтрүү шилжүүлнэ
-            $selectedItems = isset($_POST['list1Items']) ? $_POST['list1Items'] : [];
-            foreach ($selectedItems as $item) {
-                $index = array_search($item, $list1);
-                if ($index !== false) {
-                    array_splice($list1, $index,$index+1);  // Дарагдсан хүснэгтийн утга хасна
-                    $list2[] = $item;
-                }
-            }
-        } elseif (isset($_POST['moveToFirst'])) {   // Эхний хүснэгтрүү шилжүүлнэ
-            $selectedItems = isset($_POST['list2Items']) ? $_POST['list2Items'] : [];
-            foreach ($selectedItems as $item) {
-                $index = array_search($item, $list2);
-                if ($index !== false) {
-                    array_splice($list2, $index,$index+1);  // Хүснэгтдэх утга хасна 
-                    $list1[] = $item;
-                }
-            }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['moveToSecond'])) {  // 2дох хүснэгтрүү шилжүүлнэ
+      $selectedItems = isset($_POST['list1Items']) ? $_POST['list1Items'] : [];
+      foreach ($selectedItems as $item) {
+        $index = array_search($item, $list1);
+        if ($index !== false) {
+          array_splice($list1, $index, $index + 1);  // Дарагдсан хүснэгтийн утга хасна
+          $list2[] = $item;
         }
-
-        $_SESSION['list2'] = $list2;
+      }
+    } elseif (isset($_POST['moveToFirst'])) {   // Эхний хүснэгтрүү шилжүүлнэ
+      $selectedItems = isset($_POST['list2Items']) ? $_POST['list2Items'] : [];
+      foreach ($selectedItems as $item) {
+        $index = array_search($item, $list2);
+        if ($index !== false) {
+          array_splice($list2, $index, $index + 1);  // Хүснэгтдэх утга хасна 
+          $list1[] = $item;
+        }
+      }
     }
-    ?>
 
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <select id="list1" name="list1Items[]" multiple>
-            <?php
-            foreach ($list1 as $item) {
-                echo '<option value="' . $item . '">' . $item . '</option>';
-            }
-            ?>
-        </select>
+    $_SESSION['list2'] = $list2;
+  }
+  ?>
 
-        <button type="submit" name="moveToSecond">Хоёрдох хүснэгтрүү шилжүүлнэ</button>
-        <button type="submit" name="moveToFirst">Эхний хүснэгтрүү шилжүүлнэ</button>
+  <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <select id="list1" name="list1Items[]" multiple>
+      <?php
+      foreach ($list1 as $item) {
+        echo '<option value="' . $item . '">' . $item . '</option>';
+      }
+      ?>
+    </select>
 
-        <select id="list2" name="list2Items[]" multiple>
-            <?php
-            foreach ($list2 as $item) {
-                echo '<option value="' . $item . '">' . $item . '</option>';
-            }
-            ?>
-        </select>
-    </form>
+    <button type="submit" name="moveToSecond">Хоёрдох хүснэгтрүү шилжүүлнэ</button>
+    <button type="submit" name="moveToFirst">Эхний хүснэгтрүү шилжүүлнэ</button>
+
+    <select id="list2" name="list2Items[]" multiple>
+      <?php
+      foreach ($list2 as $item) {
+        echo '<option value="' . $item . '">' . $item . '</option>';
+      }
+      ?>
+    </select>
+  </form>
 
 </body>
 
